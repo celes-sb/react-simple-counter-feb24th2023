583dd6508167c8d515031a49d15cac563cee27a7
Celeste Bareiro <cnyx7@proton.me>
2023-02-25 04:23:34 +0000

    final changes in Bootstrap and CSS

--- src/js/component/secondscounter.jsx
+++ src/js/component/secondscounter.jsx
@@ -9,16 +9,15 @@ const SecondsCounter = (props) => {
 
     return (
         <>
-            <div className="container-fluid m-5 w-75 text-light bg-dark border">
-                <h1 className="row ms-4 p-2">Seconds Counter</h1>
-                <div className="row ms-4 p-1">
-                    <i className="display-5 col-1 mt-1 fa-regular fa-clock shadow-lg"></i>
-                    <h1 className="numero display-1 col-1 rounded">{props.hora3}</h1>
-                    <h1 className="numero display-1 col-1 rounded">{props.hora2}</h1>
-                    <h1 className="numero display-1 col-1 rounded">{props.hora1}</h1>
-                    <h1 className="numero display-1 col-1 rounded">{props.centenaSeconds}</h1>
-                    <h1 className="numero display-1 col-1 rounded">{props.decenaSeconds}</h1>
-                    <h1 className="numero display-1 col-1 mb-5 rounded shadow-lg ">{props.seconds}</h1>
+            <div className="container-fluid text-light bg-dark">
+                <div className="row">
+                    <i className="icono col-1 fa-regular fa-clock shadow"></i>
+                    <h1 className="numero col-1 rounded shadow-sm">{props.hora3}</h1>
+                    <h1 className="numero col-1 rounded shadow-sm">{props.hora2}</h1>
+                    <h1 className="numero col-1 rounded shadow-sm">{props.hora1}</h1>
+                    <h1 className="numero col-1 rounded shadow-sm">{props.centenaSeconds}</h1>
+                    <h1 className="numero col-1 rounded shadow-sm">{props.decenaSeconds}</h1>
+                    <h1 className="numero col-1 mb-5 rounded shadow-sm">{props.seconds}</h1>
                 </div>
             </div>
         </>
